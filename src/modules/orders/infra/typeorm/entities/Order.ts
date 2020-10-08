69e60ced636f8a094008597d1ffb988ac24bc7d4
Flavio B Santos <bzsflavio@gmail.com>
2020-10-08 14:32:27 -0300

    tt

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -11,15 +11,29 @@ import {
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  // eager tras dados do relacionamento automaticamente, mas existe um metodo para fazer isso no repositorio
+  // @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.order, {
+  //   eager: true,
+  //   cascade: true,
+  // })
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
